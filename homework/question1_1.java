800db46e096e1bef30b42d2ccd5b6c3fffa9290c
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-03-06 04:14:13 -0600

    Homework 1 Assigned

---
+++ homework/question1_1.java
@@ -0,0 +1,5 @@
+public class question1_1 {
+    public static void main(String[] args) {
+        // Using Java, write a function that will determine whether a given integer n is a prime number or not
+    }
+}
---
+++ homework/question1_2.java
@@ -0,0 +1,6 @@
+public class question1_2 {
+    // Write a function that takes in two integers and returns their GCF (Greatest Common Factor) 
+    public static void main(String[] args) {
+
+    }
+}
