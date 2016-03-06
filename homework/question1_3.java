aea8f3b7f7da9d83046597f3ac2ebef6e1f79f41
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-03-06 04:17:58 -0600

    Adding question 3

---
+++ homework/question1_3.java
@@ -0,0 +1,15 @@
+public class Computer {
+
+}
+
+
+// Computer 클래스를 디자인하기 
+// //
+// // 수업시간때 봤던 Computer 예시를 코드로 작성하기. 
+// //
+// //
+// // Write at least two other classes that inherits from Computer class
+// //
+// // How to specify that a class inherits from another:
+// //  " public class Child extends Parent "
+//
